00fdd3ee9e3d7a0a900aca5bcdb86e194ef5b89a
David Chia <david.mhchia@gmail.com>
2015-08-11 23:13:24 +0100

    Finally got scaling and translating working. Achieved so by transforming coordinates directly, instead of transforming svg canvas later on.

--- graph/svgRenderer/svgRenderer_v1.2/index.js
+++ graph/svgRenderer/svgRenderer_v1.2/index.js
@@ -5,29 +5,69 @@ var data, nodes, links, rect;
 function renderInput(file) {
     var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
 
-        if (xhr.readyState==4 && xhr.status==200) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
             data = JSON.parse(xhr.responseText);
 
             nodes = data.nodes;
             links = data.links;
             rect = data.rectDimension;
-            console.log(rect);
-
-// rendering
-// set up svgRoot
-            var svgRoot = svg("svg");
-
-            document.body.appendChild(svgRoot);  //getElementById( ) can be used to substitute body
 
 
-// set up container for graph
-
+            // transform data: instead of transforming SVG elements, transform coordinates instead
+            // also set up container for graph
             var containerDimension = {
                 width: document.body.clientWidth,
                 height: document.body.clientHeight
             }
+
+            var scales = {
+                x: (containerDimension.width / (rect.x2 - rect.x1)),
+                y: (containerDimension.height / (rect.y2 - rect.y1))
+            };
+
+
+            // scaleDown  makes the overall graph smaller and nearer to (0, 0)
+            var scaleDown = 0.9;
+
+            nodes.forEach(function (node) {
+                node.pos.x += -rect.x1;    // align top left corner of force-generated graph to (0,0)
+                node.pos.y += -rect.y1;
+
+                node.pos.x *= scales.x * scaleDown;
+                node.pos.y *= scales.y * scaleDown;
+
+                node.pos.x += containerDimension.width * (1 - scaleDown) / 2;    // add padding to x & y to centralise graph
+                node.pos.y += containerDimension.height * (1 - scaleDown) / 2;
+            });
+
+            links.forEach(function (link) {
+
+
+                link.pos.from.x += -rect.x1;
+                link.pos.from.y += -rect.y1;
+                link.pos.to.x += -rect.x1;
+                link.pos.to.y += -rect.y1;
+
+                link.pos.from.x *= scales.x * scaleDown;
+                link.pos.from.y *= scales.y * scaleDown;
+                link.pos.to.x *= scales.x * scaleDown;
+                link.pos.to.y *= scales.y * scaleDown;
+
+                link.pos.from.x += containerDimension.width * (1 - scaleDown) / 2;
+                link.pos.from.y += containerDimension.height * (1 - scaleDown) / 2;
+                link.pos.to.x += containerDimension.width * (1 - scaleDown) / 2;
+                link.pos.to.y += containerDimension.height * (1 - scaleDown) / 2;
+            });
+
+
+            // rendering
+            // set up svgRoot
+            var svgRoot = svg("svg");
+
+            document.body.appendChild(svgRoot);  //getElementById( ) can be used to substitute body
+
 
             var translate = { // -rect.x1 instead of Math.abs(rect.x1) because of the case where rect is fully inside container
                 x: -rect.x1 + (containerDimension.width / 2 - (rect.x2 - rect.x1) / 2),// first translate by smallest x, then translate by difference between centre of container and centre of rect
@@ -35,12 +75,9 @@ function renderInput(file) {
             };
 
 
-
-            var scale = {
-                x: (containerDimension.width / (rect.x2 - rect.x1)),
-                y: (containerDimension.height / (rect.y2 - rect.y1))
-            };
-
+            if (scales.x < 1 || scales.y < 1) {
+                scales.x = scales.y = Math.min(scales.x, scales.y);
+            }
 
             /* actually all the formulaes are the same, no need to split into cases
              if (rect.x1 < 0 && rect.x2 < 0) {  // and assuming x1 is always < x2
@@ -52,34 +89,48 @@ function renderInput(file) {
              }
              */
 
+            translate.x = 0;
+            translate.y = 0;
+            scales.x = 1;
+            scales.y = 1;
+
+
+            console.log("containerDimension");
+            console.log(containerDimension);
+            console.log("rect");
+            console.log(rect);
+            console.log("scale");
+            console.log(scales);
+            console.log("translate");
+            console.log(translate);
 
             svgRoot.attr("width", containerDimension.width)
                 .attr("height", containerDimension.height);     // learning: height and width should be set to the overall svg canvas, instead of "g" within. It has no effect on "g"
 
             var graph = svgRoot.append("g")
-                .attr("transform", "matrix(" + 0.5 + "," + 0 + ","  + 0 + "," + 0.5 + "," + translate.x + "," + translate.y + ")")   // scaling affects transformation
-                .attr("class", "graph")
-                .attr("buffered-rendering", "static");
+                .attr("transform", "matrix(" + scales.x + "," + 0 + "," + 0 + "," + scales.y + "," + translate.x + "," + translate.y + ")")   // scaling affects transformation
+                .attr("class", "graph");
 
 
-            console.log("Time before rendering: " + window.performance.now());
+            //console.log("Time before rendering: " + window.performance.now());
             window.performance.mark("mark_before_append");
-// rendering
 
-// render edges
-            links.forEach(function(link) {
+            // rendering
 
-                graph.append("line")
-                    .attr("x1", link.pos.from.x)   // if node1 in edge is 15, this will be nodesArr[15][0], to access x coord of node 15; internal d[0] refers to node1
-                    .attr("y1", link.pos.from.y)
-                    .attr("x2", link.pos.to.x)
-                    .attr("y2", link.pos.to.y)
-                    .attr("stroke-width", 1)
-                    .attr("stroke", "#B8B8B8 ");
-            });
+             // render edges
+             links.forEach(function(link) {
+
+             graph.append("line")
+             .attr("x1", link.pos.from.x)   // if node1 in edge is 15, this will be nodesArr[15][0], to access x coord of node 15; internal d[0] refers to node1
+             .attr("y1", link.pos.from.y)
+             .attr("x2", link.pos.to.x)
+             .attr("y2", link.pos.to.y)
+             .attr("stroke-width", 1)
+             .attr("stroke", "#B8B8B8 ");
+             });
 
 
-// render nodes
+            // render nodes
 
             nodes.forEach(function (node) {
 
@@ -100,23 +151,20 @@ function renderInput(file) {
             });
 
 
-
-
-
             window.performance.mark("mark_after_append");
 
             window.performance.measure("measure_append", "mark_before_append", "mark_after_append");
-            console.log("Time after rendering: " + window.performance.now());
+            //     console.log("Time after rendering: " + window.performance.now());
 
 
             var mark_all = window.performance.getEntriesByType("mark");
 
             var measure_all = window.performance.getEntriesByType("measure");
 
-            console.log("All marks are: ");
-            console.log(mark_all);
-            console.log("All measures are: ");
-            console.log(measure_all);
+//            console.log("All marks are: ");
+            //          console.log(mark_all);
+            //        console.log("All measures are: ");
+            //      console.log(measure_all);
 
         }
 
